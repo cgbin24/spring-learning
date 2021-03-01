0b2e0976e34d25c0ce02a93473a914477890d5f7
cgbin24 <c_being@qq.com>
2021-03-01 23:56:08 +0800

    feat:@Qualifier

--- spring-05-autowired/src/main/java/xyz/cgbin/pojo/People.java
+++ spring-05-autowired/src/main/java/xyz/cgbin/pojo/People.java
@@ -1,13 +1,22 @@
 package xyz.cgbin.pojo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.lang.Nullable;
 
 public class People {
-    @Autowired
+    // 如果显示的定义了Autowired的required属性为false，说明这个对象的值可以为空，否则不能为null
+    @Autowired(required = false)
     private Cat cat;
     @Autowired
+    @Qualifier(value = "dog")
     private Dog dog;
     private String name;
+
+    // @Nullable 标记过的值可以为空
+//    public People(@Nullable String name) {
+//        this.name = name;
+//    }
 
     public Cat getCat() {
         return cat;
